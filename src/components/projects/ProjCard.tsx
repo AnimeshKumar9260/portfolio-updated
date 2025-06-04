58f0b5e78581e18010511f21d743faf481b4c802
Animesh Kumar <animeshkumar@registerkaro.com>
2025-06-04 17:07:33 +0530

    Made project card

---
+++ src/components/projects/ProjCard.tsx
@@ -0,0 +1,26 @@
+export default function ProjCard() {
+  return (
+    <div>
+      <p className="text-gray-500">
+        Ukiyo Stays is a immersive villa rental service website where hosts can
+        rent their properties and then book a stay with the host and is a real
+        world full stack website.
+      </p>
+      <div className="flex flex-wrap items-center gap-2 my-2">
+        <p className="py-1 px-3 bg-sky-500/10 text-sm rounded-md text-gray-300">
+          Socket.io
+        </p>
+        <p className="py-1 px-3 bg-sky-500/10 text-sm rounded-md text-gray-300">
+          Express
+        </p>
+        <p className="py-1 px-3 bg-sky-500/10 text-sm rounded-md text-gray-300">
+          MongoBD
+        </p>
+        <p className="py-1 px-3 bg-sky-500/10 text-sm rounded-md text-gray-300">
+          React
+        </p>
+      </div>
+      <h3 className="text-sky-500/20 font-extrabold text-6xl">Ukiyo Stays</h3>
+    </div>
+  );
+}
